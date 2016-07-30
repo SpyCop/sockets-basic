3e11c37222d5a2a0f4c970a47df91000133f54de
SpyCop <g.c.vandenbrink@student.utwente.nl>
2016-07-30 23:34:06 +0200

    @currentUsers

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+//@currentUsers send all room's users to socket
+function sendCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketID) {
+		var userInfo = clientInfo[socketID];
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: "System",
+		text: "Current users: " + users.join(', '),
+		timestamp: moment().valueOf()
+	});
+}
+
 io.on('connection', function (socket) {
 	console.log('User connected via Socket.io');
 
@@ -37,9 +60,14 @@ io.on('connection', function (socket) {
 	});
 
 	socket.on('message', function (message) {
-		message.timestamp = moment().valueOf();
 		console.log('Message received:' + message.name + '@' + moment.utc(message.timestamp).local().format('d/m/YYYY HH:mm') +': ' +  message.text);
-		io.to(clientInfo[socket.id].room).emit('message', message);
+
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	socket.emit('message', {
